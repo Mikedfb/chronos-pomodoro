027430c37967876ad02a24d744205c1639308730
Mike Felix <mike.felix.d@gmail.com>
2025-09-22 21:43:21 -0300

    aprendendo css model

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
+import { Heading } from './components/Heading';
 import './App.css';
 
 export function App() {
   console.log('oi');
-  return <h1> Ola mundo (do App) </h1>;
+  return <Heading />;
 }
