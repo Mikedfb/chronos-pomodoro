8c039b8859c32b065f3a2a08f3c438c2ad0732a5
Mike Felix <mike.felix.d@gmail.com>
2025-09-23 11:11:17 -0300

    começando a dividir sections

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
-import { Heading } from './components/Heading';
-import './App.css';
+import './styles/theme.css';
+import './styles/global.css';
 
 export function App() {
-  console.log('oi');
-  return <Heading />;
+  return (
+    <>
+      <div className='container'>
+        <div className='content'>
+          <section></section>
+        </div>
+      </div>
+    </>
+  );
 }
