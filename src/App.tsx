827531bd7d4f7ad4acfb546071c8a3fcbb5bea8f
Mike Felix <mike.felix.d@gmail.com>
2025-12-16 18:24:56 -0300

    iniciando css modules

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { Heading } from './components/Heading';
+
 export function App() {
   return (
     <div>
       <h1>ola mundo</h1>
+      <Heading />
     </div>
   );
 }
---
+++ src/components/Heading.tsx
@@ -0,0 +1,9 @@
+import styles from './Heading.module.css';
+
+export function Heading() {
+  return (
+    <div>
+      <h1 className={styles.heading}>titulo 2</h1>
+    </div>
+  );
+}
