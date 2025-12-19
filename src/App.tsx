bb9d25df856f7623869a5d68c93de249342938a7
Mike Felix <mike.felix.d@gmail.com>
2025-12-19 18:45:43 -0300

    iniciando componente count

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
+import { Container } from './components/Container';
+import { Logo } from './components/Logo';
+import { Menu } from './components/Menu';
+
 import './styles/theme.css';
 import './styles/global.css';
-import { Container } from './components/Container';
 
 export function App() {
   return (
-    <Container>
-      <section>LOGO</section>
-    </Container>
+    <>
+      <Container>
+        <Logo />
+      </Container>
+
+      <Container>
+        <Menu />
+      </Container>
+    </>
   );
 }
