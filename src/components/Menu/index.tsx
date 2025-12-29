4348b13e633d41820fb27232470e5538f2469153
Mike Felix <mike.felix.d@gmail.com>
2025-12-29 19:08:08 -0300

    feat: integrado useEffect com useState

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,6 +1,6 @@
 import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from 'lucide-react';
 import styles from './styles.module.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 type AvailableThemes = 'dark' | 'light';
 
@@ -12,13 +12,15 @@ export function Menu() {
   ) {
     event.preventDefault();
 
-    console.log('Clicado', Date.now());
-
     setTheme(prevTheme => {
       const nextTheme = prevTheme === 'dark' ? 'light' : 'dark';
       return nextTheme;
     });
   }
+
+  useEffect(() => {
+    document.documentElement.setAttribute('data-theme', theme);
+  }, [theme]);
 
   return (
     <nav className={styles.menu}>
