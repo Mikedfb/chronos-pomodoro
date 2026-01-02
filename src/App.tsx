222a4c82646a8614317e1df854f246a740d2db93
Mike Felix <mike.felix.d@gmail.com>
2026-01-02 18:27:16 -0300

    refactor: movido formulario para um componente

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,9 @@ import { Menu } from './components/Menu';
 import './styles/theme.css';
 import './styles/global.css';
 import { CountDown } from './components/CountDown';
-import { DefaultInput } from './components/DefaultInput';
-import { Cycles } from './components/Cycles';
-import { DefaultButton } from './components/DefaultButton';
-import { PlayCircleIcon } from 'lucide-react';
+
 import { Footer } from './components/Footer';
+import { MainForm } from './components/MainForm';
 
 export function App() {
   return (
@@ -27,28 +25,7 @@ export function App() {
       </Container>
 
       <Container>
-        <form className='form'>
-          <div className='formRow'>
-            <DefaultInput
-              labelText='Task'
-              id='meuInput'
-              type='text'
-              placeholder='Digte aqui'
-            />
-          </div>
-
-          <div className='formRow'>
-            <p>Lorem ipsum dolor sit amet.</p>
-          </div>
-
-          <div className='formRow'>
-            <Cycles />
-          </div>
-
-          <div className='formRow'>
-            <DefaultButton icon={<PlayCircleIcon />} />
-          </div>
-        </form>
+        <MainForm />
       </Container>
 
       <Container>
