9f749e16eef9a6555fe625b710e677bfe6e208fb
Mike Felix <mike.felix.d@gmail.com>
2026-02-20 20:04:31 -0300

    feat/removido todos os erros do lint

--- src/workers/TimerWorkerManager.ts
+++ src/workers/TimerWorkerManager.ts
@@ -3,16 +3,14 @@ import type { TaskStateModel } from '../models/TaskStateModel';
 let instance: TimerWorkerManager | null = null;
 
 export class TimerWorkerManager {
-  private worker: Worker;
+  private readonly worker: Worker;
 
   private constructor() {
     this.worker = new Worker(new URL('./timerWorker.js', import.meta.url));
   }
 
-  static getInstance() {
-    if (!instance) {
-      instance = new TimerWorkerManager();
-    }
+  static getInstance(): TimerWorkerManager {
+    instance ??= new TimerWorkerManager();
 
     return instance;
   }
